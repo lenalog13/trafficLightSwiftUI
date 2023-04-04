f21b9552fc999e8287a221d646fc9bd71ca4f054
lenalog13 <56520937+lenalog13@users.noreply.github.com>
2023-04-04 19:27:14 +0300

    Initial Commit

---
+++ trafficLightSwiftUI/trafficLightSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightSwiftUIApp.swift
+//  trafficLightSwiftUI
+//
+//  Created by Елена Логинова on 04.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
